73252131287cceac023a532799b3283c4d75d6fc
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-08-07 14:43:08 +0000

    Adding in new posts

--- colourcode/settings.py
+++ colourcode/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-shaunadoo97-colourcoder-w0fbdz5v3nl.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'artblog',
 ]
 
