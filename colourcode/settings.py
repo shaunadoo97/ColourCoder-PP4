b77245d24fb89e2b1610eb51d7d90f0080b6a69b
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-08-27 12:56:03 +0000

    including static files

--- colourcode/settings.py
+++ colourcode/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'  # or any prefix you choose
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
