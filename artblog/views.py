0877c2144e583e6634112fae72ec18bb73ca63c5
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-09-16 17:57:22 +0000

    Adding in a redirect function after submission]

--- artblog/views.py
+++ artblog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
 
